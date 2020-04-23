14cd286b0729fd1afe3d46a444a37cb1986754ba
Maximus3333 <m.deisadze@hotmail.com>
2020-04-22 20:05:49 -0400

    1st

--- src/main/java/bsu/comp152/FXEventHandler.java
+++ src/main/java/bsu/comp152/FXEventHandler.java
@@ -9,10 +9,11 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class EventHandler {
+public class FXEventHandler {
     @FXML
     private TextField display;
     @FXML
@@ -21,13 +22,18 @@ public class EventHandler {
     private TextArea text;
 
 
-    public void readFile(ActionEvent event) throws IOException {
+    public String readFile() throws FileNotFoundException {
         var inputText = display.getText();
         File file = new File(inputText);
         Scanner sc = new Scanner(file);
         sc.useDelimiter("\\Z");
-        var buttonPressed = (Button)event.getSource();
-        text.setText(sc.next());
+        return sc.next();
+
+    }
+
+    public void buttonPressed(ActionEvent event) throws FileNotFoundException {
+        var butonPressed = (Button)event.getSource();
+        text.setText(readFile());
 
     }
 }
